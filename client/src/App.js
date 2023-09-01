0c06e8975c9a5d5389db7c2867ebf6252c8e185b
Dylan Burnham <dburnham1212@gmail.com>
2023-09-01 14:26:47 -0400

    Adding navbar and base pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Import components
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          {/* Home Page*/}
+          <Route path="/" element={<Home/>}/>
+          {/* Login Page */}
+          <Route path="login" element={<Login/>}/>
+          {/* Register Page */}
+          <Route path="register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-md navbar-light fixed-top bg-dark py-4">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light px-4" href="#">Applyify</a>
+          <button 
+            className="navbar-toggler bg-light mx-4" 
+            type="button" 
+            data-bs-toggle="collapse" 
+            data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse mx-4" id="navbarNav">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item active">
+                <Link className="nav-link text-light" to="/">Home</Link>
+              </li>
+            </ul>
+            <ul className="navbar-nav d-flex justify-content-end">
+              <li className="nav-item">
+                <Link className="nav-link text-light" to="/login">Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link text-light" to="/register">Register</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar;
