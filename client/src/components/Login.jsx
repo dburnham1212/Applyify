d670f49fa5de9c4aed375da68a9ebf4ea07cc209
Dylan Burnham <dburnham1212@gmail.com>
2023-09-01 14:55:55 -0400

    Finishing login and register forms

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-
+    <div className="d-flex justify-content-center py-5">
+      <div className="card col-10 col-sm-8 col-md-7 col-lg-6 col-xl-4 text-center">
+        <div className="card-header">
+          <h2>Login</h2>
+        </div>
+        <form className="px-3">
+          <div className="form-group pt-4">
+            <label className="form-label">Username</label>
+            <input className="form-control" type="text" placeholder="username"></input>
+          </div>
+          <div className="form-group pt-4">
+            <label className="form-label">Password</label>
+            <input className="form-control" type="password" placeholder="password"></input>
+          </div>
+          <div className="d-flex justify-content-end mx-2 my-4">
+            <button className="btn btn-dark">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,8 +2,33 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div>
-
+    <div className="d-flex justify-content-center py-5">
+      <div className="card col-10 col-sm-8 col-md-7 col-lg-6 col-xl-4 text-center">
+        <div className="card-header">
+          <h2>Register</h2>
+        </div>
+        <form className="px-3">
+          <div className="form-group pt-4">
+            <label>Username</label>
+            <input className="form-control" type="text" placeholder="username"></input>
+          </div>
+          <div className="form-group pt-4">
+            <label>Email</label>
+            <input className="form-control" type="email" placeholder="example@example.com"></input>
+          </div>
+          <div className="form-group pt-4">
+            <label>Password</label>
+            <input className="form-control" type="password" placeholder="password"></input>
+          </div>
+          <div className="form-group pt-4">
+            <label>Password Confirmation</label>
+            <input className="form-control" type="password" placeholder="confirm password"></input>
+          </div>
+          <div className="d-flex justify-content-end mx-2 my-4">
+            <button className="btn btn-dark">Register</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
