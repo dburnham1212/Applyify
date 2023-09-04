44a360c75752f1b4a6e23b43a0c1031cea42fd6c
Dylan Burnham <dburnham1212@gmail.com>
2023-09-04 19:16:55 -0400

    Editing application view page

--- client/src/components/ViewApplication.jsx
+++ client/src/components/ViewApplication.jsx
@@ -22,23 +22,42 @@ const ViewApplication = () => {
   return (
     <div>
       
-      <div className="col-11 mx-auto text-center pt-5">
-        <h1>View Application</h1>
-        <h2>Company</h2>
-        <p>{application.company}</p>
-        <h2>Title</h2>
-        <p>{application.position}</p>
-        <h2>Job Posting Link</h2>
-        <p>{application.link}</p>
-        <h2>Date Found</h2>
-        <p>{moment(application.date_job_found).format('MM/DD/YYYY')}</p>
-        <h2>Date Applied</h2>
-        <p>{moment(application.date_applied).format('MM/DD/YYYY')}</p>
-        <h2>Research Done</h2>
-        <input type="checkbox"></input>
-        <h2>LinkedIn Connection</h2>
-        <input type="checkbox"></input>
-        <h2>Notes</h2>
+      <div className="col-11 mx-auto ">
+        <div className="card my-5">
+          <div className="card-header text-center">
+            <h1>View Application</h1>
+          </div>
+          <div className="card-body">
+            <h5>Company</h5>
+            <p>{application.company}</p>
+            <h5>Title</h5>
+            <p>{application.position}</p>
+            <h5>Job Posting Link</h5>
+            <p>{application.link}</p>
+            <h5>Date Found</h5>
+            <p>{moment(application.date_job_found).format('MM/DD/YYYY')}</p>
+            <h5>Date Applied</h5>
+            <p>{moment(application.date_applied).format('MM/DD/YYYY')}</p>
+            <h5>Research Done</h5>
+            <input type="checkbox"></input>
+            <h5>LinkedIn Connection</h5>
+            <input type="checkbox"></input>
+            <div className="d-flex justify-content-end gap-2">
+              <button className="btn btn-primary">Edit</button>
+              <button className="btn btn-danger">Delete</button>
+            </div>
+          </div>
+          
+        </div>
+        <div className="card">
+          <div className="card-header">
+            <h1>Notes</h1>
+          </div>
+          <div className="card-body">
+            <h6>New Note</h6>
+            <textarea class="form-control"></textarea>
+          </div>
+        </div>
       </div>
     </div>
   )
