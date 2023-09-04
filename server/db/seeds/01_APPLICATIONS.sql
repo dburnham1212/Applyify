fb8ffd79641bf312f3831fe90f0257b696776c9d
Dylan Burnham <dburnham1212@gmail.com>
2023-09-03 22:44:06 -0400

    Adding more data to application schema

--- server/db/seeds/01_APPLICATIONS.sql
+++ server/db/seeds/01_APPLICATIONS.sql
@@ -1,3 +1,8 @@
 INSERT INTO applications (user_id, company, position, link) 
 VALUES 
-  (1, 'ABC Tech', 'Software Engineer', 'https://www.abctech.com/job123');
+  (1, 'ABC Tech', 'Software Engineer', 'https://www.abctech.com/job123'),
+  (1, 'XYZ Solutions', 'Product Manager', 'https://www.xyzsolutions.com/job456'),
+  (1, 'Acme Inc.', 'Marketing Specialist', 'https://www.acmeinc.com/job789'),
+  (2, 'Tech Innovators Inc.', 'Data Scientist', 'https://www.techinnovators.com/job321'),
+  (2, 'Global Solutions Corp.', 'Sales Manager', 'https://www.globalsolutions.com/job789'),
+  (2, 'EcoTech Environmental', 'Environmental Engineer', 'https://www.ecotechenviro.com/job555');
