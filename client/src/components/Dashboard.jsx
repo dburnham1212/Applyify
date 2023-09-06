f073fb2ac21ae8fcb50b71828ea18677b8b4485b
Dylan Burnham <dburnham1212@gmail.com>
2023-09-06 13:59:27 -0400

    More tailwind updates

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -81,7 +81,7 @@ function Dashboard() {
       }
       {/* Card view */}
       {view === viewStates.card && 
-        <div className="w-11/12 mx-auto flex gap-2 ">
+        <div className="w-11/12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-2 ">
           {applicationCardData}
         </div>
       }
