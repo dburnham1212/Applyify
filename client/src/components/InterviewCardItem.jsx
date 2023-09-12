98c944cd39a9c93e9b6ace546c0fce389164e56b
Dylan Burnham <dburnham1212@gmail.com>
2023-09-12 17:11:04 -0400

    Adding ability to edit interviews

--- client/src/components/InterviewCardItem.jsx
+++ client/src/components/InterviewCardItem.jsx
@@ -1,7 +1,13 @@
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function InterviewCardItem(props) {
+  const [interviewDate, setInterviewDate] = useState(props.dateOfInterview);
+  const [interviewer, setInterviewer] = useState(props.interviewer);
+  const [thankYouSent, setThankYouSent] = useState(props.thankYouSent);
+  const [dateSent, setDateSent] = useState(props.dateSent)
+
+  const [editMode, setEditMode] = useState(false);
 
   const deleteInterview = () => {
     axios.post(`/interviews/delete/${props.id}`)
@@ -17,20 +23,57 @@ function InterviewCardItem(props) {
   return(
     <div className="w-full rounded overflow-hidden shadow-lg bg-secondary border-2">
       <h1 className="font-bold text-xl text-center my-2 bg-secondary text-secondary-content">Interview {props.interviewCount + 1}</h1>
+      {editMode ? 
       <div className="card-body">
         <h6 className="font-bold text-secondary-content">Date Of Interview</h6>
-        <p className="text-secondary-content">{props.dateOfInterview}</p>
+        <input 
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary" 
+          type="date"
+          value={interviewDate} 
+          onChange={(e) => setInterviewDate(e.target.value)}
+        />
         <h6 className="font-bold text-secondary-content">Interviewer</h6>
-        <p className="text-secondary-content">{props.interviewer}</p>
-        <h6 className="font-bold text-secondary-content">Thank you note sent?</h6>
-        <p className="text-secondary-content">{props.thankYouSent ? "Yes" : "No"}</p>
+        <input 
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary" 
+          value={interviewer} 
+          onChange={(e) => setInterviewer(e.target.value)}
+        />
+        <div>
+          <h6 className="font-bold text-secondary-content">Thank you note sent?</h6>
+          <input 
+            type="checkbox" 
+            checked={thankYouSent}
+            onChange={(e) => (setThankYouSent(e.target.checked))}
+          />
+        </div>
         <h6 className="font-bold text-secondary-content">Date Sent</h6>
-        <p className="text-secondary-content">{props.dateSent}</p>
+        <input 
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary" 
+          type="date"
+          value={dateSent} 
+          onChange={(e) => setDateSent(e.target.value)}
+        />
         <div className="flex justify-end gap-2">
-          <button className="btn btn-dark">Edit</button>
+          <button className="btn btn-dark" onClick={() => setEditMode(false)}>Save</button>
+          <button className="btn btn-error" onClick={() => setEditMode(false)}>Cancel</button>
+        </div>
+      </div>
+      :
+      <div className="card-body">
+        <h6 className="font-bold text-secondary-content">Date Of Interview</h6>
+        <p className="text-secondary-content">{interviewDate}</p>
+        <h6 className="font-bold text-secondary-content">Interviewer</h6>
+        <p className="text-secondary-content">{interviewer}</p>
+        <h6 className="font-bold text-secondary-content">Thank you note sent?</h6>
+        <p className="text-secondary-content">{thankYouSent ? "Yes" : "No"}</p>
+        <h6 className="font-bold text-secondary-content">Date Sent</h6>
+        <p className="text-secondary-content">{dateSent}</p>
+        <div className="flex justify-end gap-2">
+          <button className="btn btn-dark" onClick={() => setEditMode(true)}>Edit</button>
           <button className="btn btn-error" onClick={() => deleteInterview()}>Delete</button>
         </div>
       </div>
+      }
     </div>
   )
 }
